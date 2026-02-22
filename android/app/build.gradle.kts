b7cef452525cb24260289894f89d8c9723416c8c
thegravity98 <pranavkothari20@gmail.com>
2026-02-22 22:05:04 +0530

    Updated sdk version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -26,7 +26,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = 31
-        targetSdk = 34
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
@@ -39,25 +39,26 @@ android {
     //     }
     // }
 
-    signingConfigs {
-        create("release") {
-            // Only configure signing if not in CI
-            if (System.getenv("CI") != "true") {
-                val storeFilePath = System.getenv("ANDROID_STORE_FILE") ?: project.property("MYAPP_RELEASE_STORE_FILE").toString()
-                println("Store file path: $storeFilePath")
-                storeFile = file(storeFilePath)
-                storePassword = System.getenv("ANDROID_STORE_PASSWORD") ?: project.property("MYAPP_RELEASE_STORE_PASSWORD").toString()
-                keyAlias = System.getenv("ANDROID_KEY_ALIAS") ?: project.property("MYAPP_RELEASE_KEY_ALIAS").toString()
-                keyPassword = System.getenv("ANDROID_KEY_PASSWORD") ?: project.property("MYAPP_RELEASE_KEY_PASSWORD").toString()
-            }
-        }
-    }
+    //signingConfigs {
+        //create("release") {
+      //      // Only configure signing if not in CI
+            //if (System.getenv("CI") != "true") {
+               // val storeFilePath = System.getenv("ANDROID_STORE_FILE") ?: project.property("MYAPP_RELEASE_STORE_FILE").toString()
+                //println("Store file path: $storeFilePath")
+                //storeFile = file(storeFilePath)
+              //  storePassword = System.getenv("ANDROID_STORE_PASSWORD") ?: project.property("MYAPP_RELEASE_STORE_PASSWORD").toString()
+           ///     keyAlias = System.getenv("ANDROID_KEY_ALIAS") ?: project.property("MYAPP_RELEASE_KEY_ALIAS").toString()
+         //       keyPassword = System.getenv("ANDROID_KEY_PASSWORD") ?: project.property("MYAPP_RELEASE_KEY_PASSWORD").toString()
+       //     }
+     //   }
+    // }
 
     buildTypes {
         getByName("release") {
             isMinifyEnabled = true
             isShrinkResources = true
-            signingConfig = if (System.getenv("CI") == "true") null else signingConfigs.getByName("release")
+            // signingConfig = if (System.getenv("CI") == "true") null else signingConfigs.getByName("release")
+            signingConfig = signingConfigs.getByName("debug")
             proguardFiles(getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
         }
     }
